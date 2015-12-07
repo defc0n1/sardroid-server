f9e8f7fc4e5c8ff5001fe348b0ac14a25031a7c5
Matti Jokitulppo <melonmanchan@gmail.com>
2015-12-07 15:23:19 +0200

    Fix wrong config key in start script

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@
 export default {
     port: process.ENV_PORT || 9000,
     https: {
-        inUse:       new Boolean(process.env.USE_HTTPS),
+        inUse:       new Boolean(process.env.USE_HTTPS || false),
         privateKey:  '/etc/letsencrypt/live/mattij.com/privkey.pem',
         cert:        '/etc/letsencrypt/live/mattij.com/cert.pem',
         ca:          '/etc/letsencrypt/live/mattij.com/fullchain.pem'
