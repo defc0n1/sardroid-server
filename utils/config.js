1ce1aa09877eac7e5d3375a62934f9183dafae98
Matti Jokitulppo <melonmanchan@gmail.com>
2015-12-07 15:20:47 +0200

    Letsencrypt path to ssl files

--- utils/config.js
+++ utils/config.js
@@ -4,9 +4,9 @@ export default {
     port: process.ENV_PORT || 9000,
     https: {
         inUse:       new Boolean(process.env.USE_HTTPS),
-        privateKey:  '',
-        cert:        '',
-        ca:          ''
+        privateKey:  '/etc/letsencrypt/live/mattij.com/privkey.pem',
+        cert:        '/etc/letsencrypt/live/mattij.com/cert.pem',
+        ca:          '/etc/letsencrypt/live/mattij.com/fullchain.pem'
     },
     peerJSOptions: {
         debug: true,
