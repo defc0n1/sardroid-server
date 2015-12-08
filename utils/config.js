6bd7dc85aaea34295e20acf0579f8386b069153e
Matti Jokitulppo <melonmanchan@gmail.com>
2015-12-08 11:20:51 +0200

    Add proxied setting to peerjs server, better way of parsing environment variables

--- utils/config.js
+++ utils/config.js
@@ -3,15 +3,16 @@
 export default {
     port: process.ENV_PORT || 9000,
     https: {
-        inUse:       new Boolean(process.env.USE_HTTPS || false),
+        inUse:       (process.env.USE_HTTPS === ' true'),
         privateKey:  process.env.PRIVATE_KEY || '/etc/letsencrypt/live/mattij.com/privkey.pem',
         cert:        process.env.CERT        || '/etc/letsencrypt/live/mattij.com/cert.pem',
         ca:          process.env.CA          || '/etc/letsencrypt/live/mattij.com/fullchain.pem'
     },
     peerJSOptions: {
-        debug: true,
-        port: process.ENV_PORT || 9000,
-        allow_discovery: true
+        debug:           true,
+        port:            process.ENV_PORT || 9000,
+        allow_discovery: true,
+        proxied:         (process.env.IS_PROXIED === 'true')
     }
 }
 
