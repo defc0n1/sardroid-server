88fb6dbc66f5aa513ede6fb1c03f4857c74607b3
Matti Jokitulppo <melonmanchan@gmail.com>
2015-12-08 11:25:21 +0200

    Fix port config parsing

--- utils/config.js
+++ utils/config.js
@@ -1,18 +1,18 @@
 'use strict';
 
 export default {
-    port: process.ENV_PORT || 9000,
+    port: process.env.PORT || 9000,
     https: {
-        inUse:       (process.env.USE_HTTPS === ' true'),
+        inUse:       (process.env.USE_HTTPS ==  'true'),
         privateKey:  process.env.PRIVATE_KEY || '/etc/letsencrypt/live/mattij.com/privkey.pem',
         cert:        process.env.CERT        || '/etc/letsencrypt/live/mattij.com/cert.pem',
         ca:          process.env.CA          || '/etc/letsencrypt/live/mattij.com/fullchain.pem'
     },
     peerJSOptions: {
         debug:           true,
-        port:            process.ENV_PORT || 9000,
+        port:            process.env.PORT || 9000,
         allow_discovery: true,
-        proxied:         (process.env.IS_PROXIED === 'true')
+        proxied:         (process.env.IS_PROXIED == 'true')
     }
 }
 
