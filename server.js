a3494be96240fa72f464e86c8187451ec254815a
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-07 10:56:32 +0200

    Add CORS support

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict';
 
-import express from 'express';
-import ip      from 'ip';
+import express    from 'express';
+import ip         from 'ip';
 import bodyParser from 'body-parser';
-import { config }  from './utils/';
-import auth from './routes/auth';
+import cors       from 'cors';
+import { config } from './utils/';
+import auth       from './routes/auth';
 
 let app = express();
 
+app.use(cors());
 app.use(bodyParser.json())
 app.use('/auth', auth);
 
