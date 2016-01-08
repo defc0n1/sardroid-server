d6304a4ddc0c3a0346accdac072323532c3c7a58
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-08 15:57:21 +0200

    Add compression suppor to Express app

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 'use strict';
 
-import express    from 'express';
-import ip         from 'ip';
-import bodyParser from 'body-parser';
-import cors       from 'cors';
-import { config } from './utils/';
-import auth       from './routes/auth';
-import jsonErr    from './middleware/jsonErr.js';
+import express     from 'express';
+import bodyParser  from 'body-parser';
+import compression from 'compression'
+import ip          from 'ip';
+import cors        from 'cors';
+import { config }  from './utils/';
+import auth        from './routes/auth';
+import jsonErr     from './middleware/jsonErr.js';
 
 let app = express();
+
+app.use(compression());
 app.use(jsonErr);
 app.use(cors());
 app.use(bodyParser.json())
