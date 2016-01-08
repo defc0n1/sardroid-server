dd40e2532653dd2bb9dc7a5cd112f96e97d2b69b
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-08 11:19:26 +0200

    Authentication and generic error types

--- utils/errorTypes.js
+++ utils/errorTypes.js
@@ -1,15 +1,32 @@
 'use strict';
 
 /*
- * Various helpful error types matching the application routes
+ * Error types for the authentication route
  */
-
-
-const AUTH  = {
-    VERIFICATION: {},
-    REGISTER: {},
-    LOGIN:  {}
+const AUTH = {
+    VERIFICATION   : {
+        USER_EXISTS    : 'user_exists',
+        NUMBER_MISSING : 'number_missing'
+    },
+    REGISTER       : {
+        NO_VERIFICATION      : 'no_verification',
+        VERIFICATION_EXPIRED : 'verification_expired',
+        VERIFICATION_USED    : 'verification_used',
+        REGISTER_FAILED      : 'register_failed',
+    },
+    LOGIN          : {
+        USER_NOT_FOUND : 'user_not_found',
+        BCRYPT_ERROR   : 'bcrypt_error',
+        WRONG_PASSWORD : 'wrong_password'
+    }
 }
 
-export { AUTH }
+/*
+ * Misc. generic error types
+ */
+const GENERIC = {
+    MISSING_PARAMS: 'missing_params'
+}
+
+export { AUTH, GENERIC }
 
