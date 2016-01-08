9a7641c7f8636674330dc06be22a67dee6f3ca63
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-08 10:51:28 +0200

    Add error types file

---
+++ utils/errorTypes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+ * Various helpful error types matching the application routes
+ */
+
+
+const AUTH  = {
+    VERIFICATION: {},
+    REGISTER: {},
+    LOGIN:  {}
+}
+
+export { AUTH }
+
