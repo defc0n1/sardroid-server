a6a559b8ae52cc359e1f0c79b18c6aa555a1dc98
Matti Jokitulppo <melonmanchan@gmail.com>
2016-01-27 21:20:38 +0200

    Add config.json interpolation to start script

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,20 @@
 #!/bin/env bash
 
+mkdir /sardroid/dist/config
+cat << EOF > /sardroid/dist/config/config.json
+{
+    production: {
+        host: "${POSTGRES_HOST}",
+        username: "${POSTGRES_USER}",
+        password: "${POSTGRES_PASSWORD}",
+        database: "${POSTGRES_DB}",
+        charset: "${POSTGRES_CHARSET}",
+        dialect: "postgres"
+    }
+}
+EOF
+
+
 npm run-script serve
 
 exit 0
