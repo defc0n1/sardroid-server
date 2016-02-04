cae9ae206a78d537561c9cff70ef7d8fd6cbea6f
Matti Jokitulppo <melonmanchan@gmail.com>
2016-02-04 16:07:32 +0200

    More sketching out contacts

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -5,7 +5,9 @@ import express from 'express';
 
 import models            from '../models';
 import { USER, GENERIC } from '../utils/errorTypes.js';
+import { connections }   from '../utils/socketIO';
 import { verifyJWT }     from '../middleware';
+
 
 let User = models.User;
 
@@ -15,9 +17,19 @@ router.post('/user/contacts', verifyJWT,  (req, res, next) => {
     let params = req.body;
 
     if  (params.contactsList) {
+
         User.findOne({ where: { id: req.user.id }})
             .then( user => {
-                return user.update({contactsList: params.contactsList });
+
+                let numberList = _.map(params.contactsList, 'phoneNumber]');
+
+                return User.findAll({
+                    attributes: ['phoneNumber'] ,
+                    where: { phoneNumber: { $in: numberList}}
+                })
+            })
+            .then( users => {
+                return user.update({contactsList: users });
             })
             .then( user => {
                 res.status(200).json(user.contactsList);
@@ -26,9 +38,25 @@ router.post('/user/contacts', verifyJWT,  (req, res, next) => {
                 console.log(err);
                 res.err(404, USER.CONTACTS.SAVE_ERROR, 'Error saving contacts list!');
             })
+
     } else {
         res.err(400, GENERIC.MISSING_PARAMS, 'Contact list is missing!');
     }
+});
+
+router.get('/user/contacts', verifyJWT,  (req, res, next) => {
+
+    User.findOne({ where: { id: req.user.id }})
+        .then( user => {
+            let numberList = _.map(user.contactsList, 'phoneNumber]');
+            return User.findAll({where: {phoneNumber: {$in: numberList}}})
+        })
+        .then( contacts => {
+            res.json({contactsList: contacts});
+        })
+        .catch(err => {
+            res.err(404, AUTH.LOGOUT.USER_NOT_FOUND, 'User not found');
+        })
 });
 
 router.get('/user/contacts/:state', verifyJWT,  (req, res, next) => {
@@ -47,15 +75,6 @@ router.get('/user/contacts/:state', verifyJWT,  (req, res, next) => {
         })
 });
 
-router.get('/user/contacts', verifyJWT,  (req, res, next) => {
-    User.findOne({ where: { id: req.user.id }})
-        .then( user => {
-            res.status(200).json({ contactslist: user.contactsList});
-        })
-        .catch(err => {
-            res.err(404, AUTH.LOGOUT.USER_NOT_FOUND, 'User not found');
-        })
-});
 
 
 export default router
