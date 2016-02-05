191b3eba6ef309a4594d5b4592b2538473f72bfe
Matti Jokitulppo <melonmanchan@gmail.com>
2016-02-05 12:29:46 +0200

    Refactor contacts route to use new user resolving middleware

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -3,60 +3,59 @@
 import _       from 'lodash';
 import express from 'express';
 
-import models            from '../models';
-import { USER, GENERIC } from '../utils/errorTypes.js';
-import { connections }   from '../utils/socketIO';
-import { verifyJWT }     from '../middleware';
+import models                     from '../models';
+import { USER, GENERIC }          from '../utils/errorTypes.js';
+import { connections }            from '../utils/socketIO';
+import { verifyJWT, resolveUser } from '../middleware';
 
 
 let User = models.User;
 
 let router = express.Router();
 
-router.post('/contacts', verifyJWT,  (req, res, next) => {
+router.post('/contacts', verifyJWT, resolveUser, (req, res, next) => {
     let params = req.body;
 
     if  (params.contactsList) {
         console.log('contact list', params.contactsList);
-        User.findOne({ where: { id: req.user.id }})
-            .then( user => {
+        let numberList = _.map(params.contactsList, 'phoneNumber');
+        console.log('numberlist', numberList);
+        // First, we reduce the sent contacts list to simply the phone numbers.
+        User.findAll({
+            attributes: ['phoneNumber'] ,
+            where: { phoneNumber: { $in: numberList}}
+        })
+        .then( registeredNumbers => {
 
-                let numberList = _.map(params.contactsList, 'phoneNumber');
-                console.log('numberlist', numberList);
-                return User.findAll({
-                    attributes: ['phoneNumber'] ,
-                    where: { phoneNumber: { $in: numberList}}
-                })
-            })
-            .then( users => {
-                console.log('users', users);
-                if (users) {
-                    let validContacts = _.filter(params.contactsList, contact => {
-                        return _.includes(users, contact);
-                    })
-                    return user.update({contactsList: validContacts });
-                }
-            })
-            .then( user => {
-                res.status(200).json(user.contactsList);
-            })
-            .catch(err => {
-                console.log(err);
-                res.err(500, USER.CONTACTS.SAVE_ERROR, 'Error saving contacts list!');
-            })
+            // Then, we find all the registered users that match the numbers that were sent for syncing.
+            console.log('users', registeredNumbers);
+
+            // Finally, we filter the sent contacts list down to those matching registered SoAR users.
+            let validContacts = _.filter(params.contactsList, contact => {
+                return _.includes(registeredNumbers, contact.phoneNumber);
+            });
+
+            console.log('validContacts', validContacts);
+
+            return req.user.update({contactsList: validContacts });
+        })
+        .then( updatedUser => {
+            res.status(200).json(updatedUser.contactsList);
+        })
+        .catch(err => {
+            console.log(err);
+            res.err(500, USER.CONTACTS.SAVE_ERROR, 'Error saving contacts list!');
+        })
 
     } else {
         res.err(400, GENERIC.MISSING_PARAMS, 'Contact list is missing!');
     }
 });
 
-router.get('/contacts', verifyJWT,  (req, res, next) => {
+router.get('/contacts', verifyJWT, resolveUser,  (req, res, next) => {
 
-    User.findOne({ where: { id: req.user.id }})
-        .then( user => {
-            let numberList = _.map(user.contactsList, 'phoneNumber]');
-            return User.findAll({where: {phoneNumber: {$in: numberList}}})
-        })
+        let numberList = _.map(req.user.contactsList, 'phoneNumber]');
+        User.findAll({where: {phoneNumber: {$in: numberList}}})
         .then( contacts => {
             res.json({contactsList: contacts});
         })
