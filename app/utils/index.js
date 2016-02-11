70b97d6f0850c27673da4667fdf3104faa9992e3
Matti Jokitulppo <melonmanchan@gmail.com>
2016-02-11 11:25:05 +0200

    Switch randomstring module to custom one

--- app/utils/index.js
+++ app/utils/index.js
@@ -3,8 +3,9 @@
 import config             from './config';
 import createPeerJS       from './createPeerJS';
 import decodeJWT          from './decodeJWT';
+import generateRandomPin  from './generateRandomPin';
 import { log, LOG_TYPES}  from './log';
 import { createSocketIO } from './socketIO';
 
-export { config, createPeerJS, decodeJWT, createSocketIO, log, LOG_TYPES }
+export { config, createPeerJS, decodeJWT, generateRandomPin, createSocketIO, log, LOG_TYPES }
 
