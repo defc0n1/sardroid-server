95ab17c85a0ecb5b7f25366b7fd36627e5e7284d
Matti Jokitulppo <melonmanchan@gmail.com>
2016-07-20 17:45:52 +0300

    Sort user by state first, then if they're offline or not

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -73,7 +73,7 @@ router.get('/contacts', verifyJWT, resolveUser,  (req, res, next) => {
             });
 
             let sortedList = _.sortBy(listWithState, contact => {
-                return contact.currentState === contactStates.OFFLINE;
+                return contact.currentState === contactStates.OFFLINE && -contact.lastSeen;
             });
 
             res.json(sortedList);
