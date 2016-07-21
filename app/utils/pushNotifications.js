6767830f4132bd84a4ee0902b91f2d478856a0a6
Matti Jokitulppo <melonmanchan@gmail.com>
2016-07-21 16:06:25 +0300

    Merge some default nofitication options

--- app/utils/pushNotifications.js
+++ app/utils/pushNotifications.js
@@ -1,13 +1,18 @@
 'use strict'
 
-import gcm      from 'node-gcm';
-import config   from './config';
+import gcm    from 'node-gcm';
+import _      from 'lodash';
+import config from './config';
 
 const sender = new gcm.Sender(config.notifications.apiToken);
+const defaultOptions = {
+    priority: 'high',
+}
 
 function sendNotification(contents, tokens) {
     return new Promise(function (resolve, reject) {
-        const message = new gcm.Message(contents);
+        const messageWithDefaults = _.defaults(contents, defaultOptions);
+        const message = new gcm.Message(messageWithDefaults);
 
         sender.send(message, { registrationTokens: tokens }, function (err, response) {
             if (err) {
