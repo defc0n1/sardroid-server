2527c05bdf95c458d6a5e2a6309a561183524515
Matti Jokitulppo <melonmanchan@gmail.com>
2016-07-22 14:45:24 +0300

    Fleshing out call route a bit

--- app/routes/call.js
+++ app/routes/call.js
@@ -7,10 +7,11 @@ import { verifyJWT, resolveUser } from '../middleware';
 import { sendNotification }       from '../utils/pushNotifications';
 
 let User = models.User;
+let Call = models.Call;
 
 let router = express.Router();
 
-router.post('/', verifyJWT, resolveUser, (req, res, next) => {
+router.post('/initiate', verifyJWT, resolveUser, (req, res, next) => {
     const params = req.body;
 
     if (params.recipientNumber) {
@@ -19,10 +20,20 @@ router.post('/', verifyJWT, resolveUser, (req, res, next) => {
             if (user) {
                 user.notifyAbout({ title: `Call from ${req.user.phoneNumber}`,
                                    body: `Call from ${req.user.phoneNumber}` } );
-                res.status(201).json({ message: 'Notified user' });
+
+                return Call.create({
+                    startedAt: Date.now()
+                });
+
             } else {
                 res.err(404, `User with number ${params.recipientNumber} not found`);
             }
+        })
+        .then(call =>  {
+            return call.addUsers([ user, req.params.user ]);
+        })
+        .then(callWithUsers => {
+            return res.status(201).json(callWithUsers);
         })
         .catch(err => {
             res.err(500, 'Something went wrong!');
@@ -30,6 +41,29 @@ router.post('/', verifyJWT, resolveUser, (req, res, next) => {
 
     } else {
         res.err(400, GENERIC.MISSING_PARAMS, 'Recipient number is missing!');
+    }
+});
+
+router.post('/:callID/end', verifyJWT, resolveUser, (req, res, next) => {
+    const id = req.params.callID;
+    const finalStatus = req.params.finalStatus;
+
+    if (id && finalStatus) {
+        Call.findById(id)
+        .then(call => {
+            call.update({
+                endedAt: Date.now(),
+                finalStatus
+            })
+        })
+        .then(updatedCall => {
+            res.status(200).json(updatedCall);
+        })
+        .catch(err => {
+            res.err(500, 'Something went wrong!');
+        });
+    } else {
+        res.err(400, GENERIC.MISSING_PARAMS, 'Call ID or finalStatus is missing!');
     }
 });
 
